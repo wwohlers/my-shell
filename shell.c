15fb15af191aeab328dd361597f6c6d69fe705d4
Bill Wohlers <wohlers.w@northeastern.edu>
2022-10-27 18:29:39 -0400

    Mostly done

--- shell.c
+++ shell.c
@@ -5,72 +5,300 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <assert.h>
+#include <fcntl.h>
 
 #define MAX_LINE 256
 
+char **prev_line;
+
 /**
- * @brief Executes a single command.
+ * @brief Empty an array by setting its contents to null.
  *
- * @param args Full set of arguments, including the program name.
- * @return pid_t process id of the new launched process
+ * @param arr The array to empty.
+ * @param size The size of the array.
  */
-pid_t launch(char **args)
+void empty_arr(char **arr, int size)
 {
-  pid_t pid = fork();
-  if (pid == 0)
+  for (int i = 0; i < size; i++)
   {
-    if (execvp(args[0], args) == -1)
+    arr[i] = NULL;
+  }
+}
+
+int execute_line(char **tokens);
+void sub_prev(char **tokens);
+
+/**
+ * @brief Execute a non-builtin command and accounts for redirection.
+ *
+ * @param tokens all tokens in the command line
+ * @param start start of the relevant command
+ * @param end end of the relevant command
+ */
+void execute(char **tokens, int start, int end)
+{
+  char *cmd[MAX_LINE] = {NULL};
+  int cmd_idx = 0;
+  for (int i = start; i < end; i++)
+  {
+    if (strcmp(tokens[i], ">") == 0)
     {
-      printf("%s: command not found\n", args[0]);
+      int fd = open(tokens[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+      dup2(fd, 1);
+      close(fd);
+      i++;
     }
-    exit(1);
+    else if (strcmp(tokens[i], "<") == 0)
+    {
+      int fd = open(tokens[i + 1], O_RDONLY);
+      dup2(fd, 0);
+      close(fd);
+      i++;
+    }
+    else
+    {
+      cmd[cmd_idx] = tokens[i];
+      cmd_idx++;
+    }
+  }
+  cmd[cmd_idx] = NULL;
+  if (execvp(cmd[0], cmd) == -1)
+  {
+    printf("%s: command not found\n", cmd[0]);
+  }
+  exit(1);
+}
+
+/**
+ * @brief Changes working directory.
+ *
+ * @param tokens all tokens in the command
+ * @return int whether to exit the shell
+ */
+void cd(char **tokens)
+{
+  if (tokens[1] == NULL)
+  {
+    chdir(getenv("HOME"));
   }
   else
   {
-    return pid;
+    chdir(tokens[1]);
   }
 }
 
 /**
- * @brief Executes a command or combination of commands separated by tokens like ">" or "|".
+ * @brief Executes a script.
  *
- * @param tokens list of tokens to execute
+ * @param tokens tokens in the command
  */
-void execute(char **tokens)
+int source(char **tokens)
 {
-  pid_t pid = launch(tokens);
-  waitpid(pid, NULL, 0);
+  if (tokens[1] == NULL)
+  {
+    printf("source: must provide file\n");
+    return 0;
+  }
+  FILE *fp = fopen(tokens[1], "r");
+  if (fp == NULL)
+  {
+    printf("source: %s: No such file or directory\n", tokens[1]);
+    return 0;
+  }
+  char line[MAX_LINE];
+  while (fgets(line, MAX_LINE, fp) != NULL)
+  {
+    char **tokens = get_tokens(line);
+    sub_prev(tokens);
+    if (execute_line(prev_line) == 1)
+    {
+      return 1;
+    }
+    free_tokens(tokens);
+  }
+  return 0;
 }
 
 /**
  * @brief Executes a line of input as tokens.
  *
  * @param tokens list of tokens (input to the shell)
+ * @param end index of the end of the command
+ * @return int whether to exit the shell
  */
-void execute_input(char **tokens)
+int execute_command(char **tokens, int end)
 {
-  char *current[MAX_LINE]; // current command, ending at ; or \0
-  int token_idx = 0;
-  int current_idx = 0;
+  int token_idx = end - 1;
+  if (end > 0)
+  {
+    if (strcmp(tokens[0], "exit") == 0)
+    {
+      return 1;
+    }
+    else if (strcmp(tokens[0], "cd") == 0)
+    {
+      cd(tokens);
+      return 0;
+    }
+    else if (strcmp(tokens[0], "source") == 0)
+    {
+      return source(tokens);
+    }
+    else if (strcmp(tokens[0], "help") == 0)
+    {
+      printf("cd: change directory, source: execute a script, exit: exit the shell, prev: execute the previous command, help: display this message\n");
+      return 0;
+    }
+  }
   while (1)
   {
-    if (tokens[token_idx] == NULL || *tokens[token_idx] == ';')
+    if (token_idx == 0)
     {
-      current[current_idx] = NULL;
-      current_idx = 0;
-      execute(current);
-      if (tokens[token_idx] == NULL)
+      // just run command up to len
+      pid_t pid = fork();
+      if (pid == 0)
+      {
+        execute(tokens, 0, end);
+      }
+      else
+      {
+        waitpid(pid, NULL, 0);
+      }
+      return 0;
+    }
+    if (strcmp(tokens[token_idx], "|") == 0)
+    {
+      // run command from token_idx to len, pipe results to next recursive command
+      pid_t pid_a = fork();
+      if (pid_a == 0)
+      {
+        // create pipe
+        int pipefd[2];
+        pipe(pipefd);
+        pid_t pid_b = fork();
+        if (pid_b == 0)
+        {
+          // close read end
+          close(pipefd[0]);
+          // redirect stdout to write end
+          dup2(pipefd[1], 1);
+          // close write end
+          close(pipefd[1]);
+          // execute command
+          execute_command(tokens, token_idx);
+          exit(0);
+        }
+        else
+        {
+          // child a
+          // close write end
+          close(pipefd[1]);
+          // redirect stdin to read end
+          dup2(pipefd[0], 0);
+          // close read end
+          close(pipefd[0]);
+          // execute command
+          waitpid(pid_b, NULL, 0);
+          execute(tokens, token_idx + 1, end);
+          exit(0);
+        }
+      }
+      else
+      {
+        waitpid(pid_a, NULL, 0);
+      }
+      return 0;
+    }
+    token_idx--;
+  }
+  return 0;
+}
+
+/**
+ * @brief Execute a line of input by separating by semicolons and executing each command.
+ *
+ * @param tokens
+ * @return int whether to exit the shell
+ */
+int execute_line(char **tokens)
+{
+  int i = 0;
+  char *cmd[MAX_LINE] = {NULL};
+  int cmd_idx = 0;
+  while (1)
+  {
+    if (tokens[i] == NULL || strcmp(tokens[i], ";") == 0)
+    {
+      if (execute_command(cmd, cmd_idx) == 1)
+      {
+        return 1;
+      }
+      cmd_idx = 0;
+      empty_arr(cmd, MAX_LINE);
+      if (tokens[i] == NULL)
       {
         break;
       }
     }
     else
     {
-      current[current_idx] = tokens[token_idx];
-      current_idx++;
+      cmd[cmd_idx] = tokens[i];
+      cmd_idx++;
+    }
+    i++;
+  }
+  return 0;
+}
+
+/**
+ * @brief Substitute the previous command in for the "prev" token.
+ *
+ * @param tokens
+ */
+void sub_prev(char **tokens)
+{
+  // allocate temp array of strings
+  char **new = calloc(MAX_LINE, sizeof(char *));
+  for (int i = 0; i < MAX_LINE; i++)
+  {
+    new[i] = malloc(sizeof(char *));
+  }
+
+  // copy tokens to new array, substituting for temp
+  int token_idx = 0;
+  int new_idx = 0;
+  while (tokens[token_idx] != NULL && new_idx < MAX_LINE - 1)
+  {
+    if (strcmp(tokens[token_idx], "prev") == 0)
+    {
+      // copy prev command into new
+      int prev_idx = 0;
+      while (prev_line[prev_idx] != NULL && new_idx < MAX_LINE - 1)
+      {
+        strcpy(new[new_idx], prev_line[prev_idx]);
+        prev_idx++;
+        new_idx++;
+      }
+    }
+    else
+    {
+      strcpy(new[new_idx], tokens[token_idx]);
+      new_idx++;
     }
     token_idx++;
   }
+  new[new_idx] = NULL;
+
+  // free the strings in prev_line
+  int prev_idx = 0;
+  while (prev_line[prev_idx] != NULL)
+  {
+    free(prev_line[prev_idx]);
+    prev_idx++;
+  }
+
+  prev_line = new;
 }
 
 /**
@@ -84,12 +312,16 @@ void loop()
     char input[MAX_LINE];
     printf("shell $ ");
     char *eof = fgets(input, MAX_LINE, stdin);
-    if (eof == NULL || strcmp(input, "exit\n") == 0)
+    if (eof == NULL)
     {
       break;
     }
     char **tokens = get_tokens(input);
-    execute_input(tokens);
+    sub_prev(tokens);
+    if (execute_line(prev_line) == 1)
+    {
+      break;
+    }
     free_tokens(tokens);
   }
 }
@@ -99,8 +331,10 @@ void loop()
  */
 int main(int argc, char **argv)
 {
+  prev_line = calloc(MAX_LINE, sizeof(char *));
   printf("Welcome to mini-shell.\n");
   loop();
   printf("Bye bye.\n");
+  free(prev_line);
   return 0;
 }
--- tokens.c
+++ tokens.c
@@ -14,56 +14,61 @@ static int read_string(const char *input, char *buf);
 static void init_tokens();
 static void add_token(const char *token);
 
-
-char **get_tokens(const char *input) {
+char **get_tokens(const char *input)
+{
   char buf[256];
   unsigned int i = 0;
   unsigned int b = 0;
 
   init_tokens();
 
-  while (input[i] != 0) {
-    switch (input[i]) {
-      case ' ':
-      case '\n':
-      case '\t':
-        if (b > 0) {
-          buf[b] = '\0';
-          add_token(buf);
-          b = 0;
-        }
-        break;
-      case '(':
-      case ')':
-      case '&':
-      case '>':
-      case '<':
-      case '|':
-      case ';':
-        if (b > 0) {
-          buf[b] = '\0';
-          add_token(buf);
-          b = 0;
-        }
-        buf[0] = input[i];
-        buf[1] = '\0';
+  while (input[i] != 0)
+  {
+    switch (input[i])
+    {
+    case ' ':
+    case '\n':
+    case '\t':
+      if (b > 0)
+      {
+        buf[b] = '\0';
         add_token(buf);
-        break;
-      case '"':
-        ++i; // skip the quote
-        unsigned int len = read_string(&input[i], &buf[b]);
-        b += len;
-        i += len;
-        break;
-      default:
-        buf[b] = input[i];
-        ++b;
+        b = 0;
+      }
+      break;
+    case '(':
+    case ')':
+    case '&':
+    case '>':
+    case '<':
+    case '|':
+    case ';':
+      if (b > 0)
+      {
+        buf[b] = '\0';
+        add_token(buf);
+        b = 0;
+      }
+      buf[0] = input[i];
+      buf[1] = '\0';
+      add_token(buf);
+      break;
+    case '"':
+      ++i; // skip the quote
+      unsigned int len = read_string(&input[i], &buf[b]);
+      b += len;
+      i += len;
+      break;
+    default:
+      buf[b] = input[i];
+      ++b;
     }
     ++i;
   }
 
   // add the last remaining token, if any
-  if (b > 0) {
+  if (b > 0)
+  {
     buf[b] = 0;
     add_token(buf);
   }
@@ -71,10 +76,12 @@ char **get_tokens(const char *input) {
   return tokens;
 }
 
-void free_tokens(char **tokens) {
+void free_tokens(char **tokens)
+{
   char **iter = tokens;
 
-  while (*iter != NULL) {
+  while (*iter != NULL)
+  {
     free(*iter);
     ++iter;
   }
@@ -82,9 +89,11 @@ void free_tokens(char **tokens) {
   free(tokens);
 }
 
-int read_string(const char *input, char *buf) {
+int read_string(const char *input, char *buf)
+{
   unsigned int bytes = 0;
-  while (*input && *input != '"') {
+  while (*input && *input != '"')
+  {
     *buf = *input;
     ++bytes;
     ++buf;
@@ -95,7 +104,8 @@ int read_string(const char *input, char *buf) {
   return bytes;
 }
 
-void init_tokens() {
+void init_tokens()
+{
   tokens_size = 0;
   tokens_capacity = CAP_STEP;
   tokens = malloc(sizeof(char *) * tokens_capacity);
@@ -103,17 +113,20 @@ void init_tokens() {
   tokens[0] = NULL;
 }
 
-void grow_tokens() {
+void grow_tokens()
+{
   tokens_capacity += CAP_STEP;
   tokens = realloc(tokens, sizeof(char *) * tokens_capacity);
   assert(tokens != NULL);
 }
 
-int tokens_full() {
+int tokens_full()
+{
   return tokens_size + 1 == tokens_capacity;
 }
 
-void add_token(const char* token) {
+void add_token(const char *token)
+{
   assert(token != NULL);
   if (tokens_full())
     grow_tokens();
@@ -123,5 +136,3 @@ void add_token(const char* token) {
   ++tokens_size;
   tokens[tokens_size] = NULL;
 }
-
-
